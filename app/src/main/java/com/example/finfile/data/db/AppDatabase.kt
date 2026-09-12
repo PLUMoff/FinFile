af8b9d014696a59ba0c64d8084fd4c6acd9c3b6b
PLUMoff <plumoff2007@yandex.ru>
2026-09-12 21:25:29 +0300

    Create AppDatabase.kt

---
+++ app/src/main/java/com/example/finfile/data/db/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.finfile.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [FinanceEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun financeDao(): FinanceDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun get(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "finfile.db"
+                ).build().also { INSTANCE = it }
+            }
+    }
+}
